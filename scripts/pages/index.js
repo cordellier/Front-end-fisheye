7f50634974dacaccf6d04ea27bb4b4eff9e08d4d
Guillaume <cordellier.guillaume@gmx.com>
2023-11-30 15:18:08 +0100

    Création page d'acceuil
    - JS de la page
    - CSS de la page
    - HTML modification
    - Ajouts des photos (webp)

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,35 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+import { createUserCardDOM } from "../templates/photographer.js";
+
+async function getPhotographers() {
+  try {
+    const response = await fetch("data/photographers.json");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    const data = await response.json();
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching photographers data:", error);
+  }
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function displayData(photographers, photographersSection) {
+  photographers.forEach((photographer) => {
+    const userCardDOM = createUserCardDOM(photographer);
+
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  const { photographers } = await getPhotographers();
+  const photographersSection = document.querySelector(".photographer_section");
+
+  displayData(photographers, photographersSection);
+  addKeyDownNavigation(photographersSection);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,103 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+// Constante
+const ClassPhotographerCard = "photographer_picture";
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+// Utiliser un objet pour regrouper les fonctions utilitaires
+export const DOMUtils = {
+  createImage: (src, alt) => {
+    try {
+      const img = document.createElement("img");
+      img.setAttribute("src", src);
+      img.setAttribute("alt", alt);
+      return img;
+    } catch (error) {
+      console.error(
+        `Erreur lors de la création de l'image. Détails : ${error}`
+      );
+      return null;
     }
-    return { name, picture, getUserCardDOM }
+  },
+
+  createHeading: (headingType, text) => {
+    const heading = document.createElement(headingType);
+    heading.textContent = text;
+    return heading;
+  },
+
+  createParagraph: (text) => {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    return paragraph;
+  },
+
+  createCardContainer: (children) => {
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add(ClassPhotographerCard);
+    children.forEach((child) => {
+      imgContainer.appendChild(child);
+    });
+    return imgContainer;
+  },
+
+  createLinkWithHref: (href) => {
+    const link = document.createElement("a");
+    link.setAttribute("href", href);
+    return link;
+  },
+};
+
+// Fonction principale pour créer la carte d'un photographe
+export function createPhotographerCard(data) {
+  const { name, portrait, city, country, tagline, price, id } = data;
+  const picturePath = `assets/photographers/${portrait}`;
+
+  const img = DOMUtils.createImage(picturePath, name);
+  if (!img) {
+    return null;
+  }
+
+  const h2 = DOMUtils.createHeading("h2", name);
+  const h3 = DOMUtils.createHeading("h3", `${city}, ${country}`);
+  const tag = DOMUtils.createParagraph(tagline);
+  const prix = DOMUtils.createParagraph(`${price}€/jour`);
+  const imgContainer = DOMUtils.createCardContainer([img]);
+
+  const content = [imgContainer, h2, h3, tag, prix];
+  const article = createArticleWithLink(id, content, name);
+
+  return article;
 }
+
+// Fonction utilitaire pour créer un article avec un lien
+function createArticleWithLink(id, content, name) {
+  const article = document.createElement("article");
+  const link = DOMUtils.createLinkWithHref(`photographer.html?id=${id}`);
+  const ariaLabel = `View Photographer Profile: ${name}`;
+  link.setAttribute("aria-label", ariaLabel);
+
+  content.forEach((element) => {
+    link.appendChild(element);
+  });
+
+  article.appendChild(link);
+  return article;
+}
+
+// Fonction pour créer le DOM d'une carte utilisateur (utilise la fonction principale)
+export function createUserCardDOM(data) {
+  return createPhotographerCard(data);
+}
+
+// Fonction permettant de retourner sur l'acceuil avec "Alt"
+function redirectToHome() {
+  window.location.href = "index.html";
+}
+
+document.addEventListener("keydown", function (event) {
+  var altKeyPressed = event.altKey;
+
+  if (altKeyPressed) {
+    redirectToHome();
+
+    document.querySelector(".logo").style.transform = "scale(1.1)";
+  }
+});
